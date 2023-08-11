9cab5455f5fb21f262192bf68e1d7419d87b85fc
Sh9hid <abdullah.cool1@gmail.com>
2023-08-11 17:19:23 +0530

    declare global variables ball_x, ball_y

--- pong/main.py
+++ pong/main.py
@@ -1,5 +1,17 @@
 import pygame, sys
 
+def ball_animation():
+    global ball_speed_x, ball_speed_y
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_speed_x *= -1
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1 
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -27,16 +39,7 @@ while True:
             pygame.quit()
             sys.exit()
 
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
-
-    if ball.top <= 0 or ball.bottom >= screen_height:
-        ball_speed_y *= -1
-    if ball.left <= 0 or ball.right >= screen_width:
-        ball_speed_x *= -1
-
-    if ball.colliderect(player) or ball.colliderect(opponent):
-        ball_speed_x *= -1 
+    ball_animation()
 
     # Visuals
     screen.fill(bg_color)
