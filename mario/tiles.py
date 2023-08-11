c33a0b3db53d38b3eff457940bb18be621ba66c9
Sh9hid <abdullah.cool1@gmail.com>
2023-08-11 16:17:35 +0530

    player input

--- mario/tiles.py
+++ mario/tiles.py
@@ -1,3 +1,4 @@
+from typing import Any
 import pygame
 
 class Tile(pygame.sprite.Sprite):
@@ -6,3 +7,7 @@ class Tile(pygame.sprite.Sprite):
         self.image = pygame.Surface((size, size))
         self.image.fill('grey')
         self.rect = self.image.get_rect(topleft = pos)
+
+
+    def update(self, x_shift):
+        self.rect.x += x_shift
