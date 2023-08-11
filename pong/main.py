71dc1c6e171253bd6a0f0af9ab98042f2ee06d1b
Sh9hid <abdullah.cool1@gmail.com>
2023-08-11 17:41:44 +0530

    random, restart

--- pong/main.py
+++ pong/main.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random
 
 def ball_animation():
     global ball_speed_x, ball_speed_y
@@ -8,7 +8,7 @@ def ball_animation():
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
     if ball.left <= 0 or ball.right >= screen_width:
-        ball_speed_x *= -1
+        ball_restart()
 
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1 
@@ -30,7 +30,11 @@ def opponent_ai():
     if opponent.bottom >= screen_height:
         opponent.bottom = screen_height
     
-    
+def ball_restart():
+    global ball_speed_x, ball_speed_y
+    ball.center = (screen_width/2, screen_height/2)
+    ball_speed_y *= random.choice((1, -1))
+    ball_speed_x *= random.choice((1, -1))
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -48,8 +52,8 @@ opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 bg_color = pygame.Color('grey12')
 light_grey = (200,200,200)
 
-ball_speed_x = 7
-ball_speed_y = 7
+ball_speed_x = 7 * random.choice((-1, 1))
+ball_speed_y = 7 * random.choice((-1, 1))
 player_speed = 0
 opponent_speed = 7
 
