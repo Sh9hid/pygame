7c9bb38db3472ca16b55682fadc92ec8f711683f
Sh9hid <abdullah.cool1@gmail.com>
2023-08-11 18:12:16 +0530

    score update

--- pong/main.py
+++ pong/main.py
@@ -1,14 +1,20 @@
 import pygame, sys, random
 
 def ball_animation():
-    global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y, player_score, opponent_score
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
-    if ball.left <= 0 or ball.right >= screen_width:
-        ball_restart()
+
+    if ball.left <= 0:
+        player_score += 1
+        ball_start()
+    
+    if ball.right >= screen_width:
+        opponent_score += 1
+        ball_start() 
 
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1 
@@ -30,7 +36,7 @@ def opponent_ai():
     if opponent.bottom >= screen_height:
         opponent.bottom = screen_height
     
-def ball_restart():
+def ball_start():
     global ball_speed_x, ball_speed_y
     ball.center = (screen_width/2, screen_height/2)
     ball_speed_y *= random.choice((1, -1))
@@ -49,13 +55,20 @@ ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
 player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
 opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 
+# colors
 bg_color = pygame.Color('grey12')
 light_grey = (200,200,200)
 
+# Game variables
 ball_speed_x = 7 * random.choice((-1, 1))
 ball_speed_y = 7 * random.choice((-1, 1))
 player_speed = 0
 opponent_speed = 7
+
+# Text variables
+player_score = 0
+opponent_score = 0
+game_font = pygame.font.Font('freesansbold.ttf', 32)
 
 while True:
     # Handling input
@@ -88,6 +101,11 @@ while True:
     pygame.draw.ellipse(screen, light_grey, ball)
     pygame.draw.aaline(screen, light_grey, (screen_width/2,0), (screen_width/2,screen_height))
 
-    # updating the window
+    player_text = game_font.render(f"{player_score}", False, light_grey)
+    screen.blit(player_text,(660, 470))
+
+    opponent_text = game_font.render(f"{opponent_score}", False, light_grey)
+    screen.blit(opponent_text,(600, 470))
+
     pygame.display.flip()
     clock.tick(60)
